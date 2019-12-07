720f812b78065f99750ce9e79bac0ede81e4adbe
Maciej Wasiak <maciek.wasiak44@gmail.com>
2019-12-07 17:23:15 +0100

    fxml added

--- src/zad1/Main.java
+++ src/zad1/Main.java
@@ -1,4 +1,25 @@
 package zad1;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("THREADPOOL");
+        primaryStage.setScene(new Scene(root, 590, 400));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
 }
